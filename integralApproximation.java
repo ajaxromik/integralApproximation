0418ea6b74bcc7d5fe483c54658852003385d506
Ajax <50806767+ajaxromik@users.noreply.github.com>
2022-12-06 19:21:14 -0500

    Add files via upload

--- integralApproximation.java
+++ integralApproximation.java
@@ -3,33 +3,35 @@ public class integralApproximation {
 
 	public static void main(String[] args) {
 		
-		double subintervals = Math.pow(2, 31); //it's only a double so i can use pow()
-		System.out.println(leftApprox(subintervals));
-		System.out.println(rightApprox(subintervals));
+		double subintervals = Math.pow(2, 30); //it's only a double so i can use pow()
+		double x0 = 0; //starting position
+		double intervalLength = 1; //length of the entire interval
+		System.out.println(leftApprox(intervalLength, subintervals, x0));
+		System.out.println(rightApprox(intervalLength, subintervals, x0));
 		
 	}
 	
-	public static double leftApprox(double subintervals) {
+	public static double leftApprox(double intervalLength, double subintervals, double x0) {
 		double sigma = 0;
 		
 		for(int i = 0; i < subintervals; i++) {
-			sigma += function(i / subintervals);
+			sigma += function(i * intervalLength / subintervals + x0);
 		}
 		
 		return sigma / subintervals;
 	}
 	
-	public static double rightApprox(double subintervals) {
+	public static double rightApprox(double intervalLength, double subintervals, double x0) {
 		double sigma = 0;
 		
 		for(int i = 1; i <= subintervals; i++) {
-			sigma += function(i / subintervals);
+			sigma += function(i * intervalLength / subintervals + x0);
 		}
 		
 		return sigma / subintervals;
 	}
 	
-	public static double function(double x) {
+	public static double function(double x) { //easily changeable function
 		return x * x;
 	}
 
